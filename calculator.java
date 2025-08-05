5c561d4bb5d1476bf191836b1a72042b0e404498
Keshav Agrawal <59056315+keshav-agrawal595@users.noreply.github.com>
2025-08-05 16:24:13 +0530

    updated multiplication function

--- calculator.java
+++ calculator.java
@@ -1,35 +1,16 @@
 import java.util.*;
 
 public class calculator {
-    public static void summ(int a, int b){
-        System.out.println(a+b);
-    }
-    public static void sub(int a, int b){
-        System.out.println(a-b);
-    }
     public static void multi(int a, int b){
         System.out.println(a*b);
     }
-    public static void div(int a, int b){
-        System.out.println(a/b);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         String c = sc.next();
-        if(c == "A"){
-            summ(a, b);
-        }
-        else if(c == "S"){
-            sub(a, b);
-        }
-        else if(c == "M"){
+        if(c == "M"){
             multi(a, b);
-        }
-        else if(c == "D"){
-            div(a, b);
         }
         sc.close();
     }
