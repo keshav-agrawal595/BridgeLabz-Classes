94fc1d565de2d55b43c2b75f80abee00746e3152
Keshav Agrawal <59056315+keshav-agrawal595@users.noreply.github.com>
2025-08-05 16:23:24 +0530

    subtraction function modified

--- calculator.java
+++ calculator.java
@@ -1,17 +1,8 @@
 import java.util.*;
 
 public class calculator {
-    public static void summ(int a, int b){
-        System.out.println(a+b);
-    }
     public static void subb(int a, int b){
         System.out.println(a-b);
-    }
-    public static void multi(int a, int b){
-        System.out.println(a*b);
-    }
-    public static void div(int a, int b){
-        System.out.println(a/b);
     }
 
     public static void main(String[] args) {
@@ -19,17 +10,8 @@ public class calculator {
         int a = sc.nextInt();
         int b = sc.nextInt();
         String c = sc.next();
-        if(c == "A"){
-            summ(a, b);
-        }
-        else if(c == "S"){
+        if(c == "S"){
             subb(a, b);
-        }
-        else if(c == "M"){
-            multi(a, b);
-        }
-        else if(c == "D"){
-            div(a, b);
         }
         sc.close();
     }
