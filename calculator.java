b60adf26f442a6b3f829c2576fc7cf68f7fd6ac5
keshav-agrawal595 <keshavagr595@gmail.com>
2025-08-05 16:21:33 +0530

    divide function added

--- calculator.java
+++ calculator.java
@@ -1,15 +1,6 @@
 import java.util.*;
 
 public class calculator {
-    public static void summ(int a, int b){
-        System.out.println(a+b);
-    }
-    public static void subb(int a, int b){
-        System.out.println(a-b);
-    }
-    public static void multi(int a, int b){
-        System.out.println(a*b);
-    }
     public static void divid(int a, int b){
         System.out.println(a/b);
     }
@@ -19,16 +10,7 @@ public class calculator {
         int a = sc.nextInt();
         int b = sc.nextInt();
         String c = sc.next();
-        if(c == "A"){
-            summ(a, b);
-        }
-        else if(c == "S"){
-            subb(a, b);
-        }
-        else if(c == "M"){
-            multi(a, b);
-        }
-        else if(c == "D"){
+        if(c == "D"){
             divid(a, b);
         }
         sc.close();
