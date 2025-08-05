40af409fc0fc8b4d95e4665f73d289be03034ebe
Sarvagya Saxena <sarvagyasaxena2006@gmail.com>
2025-08-05 15:21:49 +0530

    Final commit

--- calculator.java
+++ calculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class calculator {
     public static void sum(int a, int b){
         System.out.println(a+b);
@@ -5,6 +7,29 @@ public class calculator {
     public static void sub(int a, int b){
         System.out.println(a-b);
     }
+    public static void mult(int a, int b){
+        System.out.println(a*b);
+    }
+    public static void div(int a, int b){
+        System.out.println(a/b);
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String c = sc.nextLine();
+        if(c == "A"){
+            sum(a, b);
+        }
+        if(c == "S"){
+            sum(a, b);
+        }
+        if(c == "M"){
+            sum(a, b);
+        }
+        if(c == "D"){
+            sum(a, b);
+        }
     }
 }
