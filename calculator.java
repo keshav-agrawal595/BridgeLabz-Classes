15a9d400ed648b30128714eb416c305ce6da4ce3
keshav-agrawal595 <keshavagr595@gmail.com>
2025-08-05 15:17:24 +0530

    Add & sub function added

--- calculator.java
+++ calculator.java
@@ -2,6 +2,9 @@ public class calculator {
     public static void sum(int a, int b){
         System.out.println(a+b);
     }
+    public static void sub(int a, int b){
+        System.out.println(a-b);
+    }
     public static void main(String[] args) {
     }
 }
